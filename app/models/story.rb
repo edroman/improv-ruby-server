c0435b535b2c85639f3fcb3271f477b332325bb7
Ed Roman <edro@Ed-MacBook-Air.local>
2012-04-12 21:20:44 -0700

    activeadmin improvements

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,6 @@
 class Story < ActiveRecord::Base
   belongs_to :team						          # many-to-one: Each story has exactly one team
   has_many :users, :through => :teams		# for convenience, can access users directly
+
+  accepts_nested_attributes_for :users, :allow_destroy => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :teams, :join_table => :users_teams			# many-to-many
   has_many :stories, :through => :teams                           # For convenience, can access stories directly
+
+  accepts_nested_attributes_for :teams, :allow_destroy => true
+  accepts_nested_attributes_for :stories, :allow_destroy => true
 end
