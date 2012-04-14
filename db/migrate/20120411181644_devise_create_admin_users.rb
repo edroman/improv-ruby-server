c4876aa0b34a26b0c58fa7e31e40a9958c696552
Ed Roman <edro@Ed-MacBook-Air.local>
2012-04-13 18:28:12 -0700

    testing

--- db/migrate/20120411181644_devise_create_admin_users.rb
+++ db/migrate/20120411181644_devise_create_admin_users.rb
@@ -41,10 +41,12 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
     end
 
     # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+#    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
 
-    add_index :admin_users, :email,                :unique => true
-    add_index :admin_users, :reset_password_token, :unique => true
+#    add_index :admin_users, :email,                :unique => true
+#    add_index :admin_users, :reset_password_token, :unique => true
+
+
     # add_index :admin_users, :confirmation_token,   :unique => true
     # add_index :admin_users, :unlock_token,         :unique => true
     # add_index :admin_users, :authentication_token, :unique => true
