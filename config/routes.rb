76e8e12e139c7068e4d67a0897c12a1538e5a87d
Ed Roman <edward.w.roman@gmail.com>
2012-04-14 02:39:26 -0700

    ActiveAdmin route set

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Improv::Application.routes.draw do
 
   resources :users
 
-  root :to => "users#index"
+  root :to => "admin/dashboard#index"
 
 
   # The priority is based upon order of creation:
