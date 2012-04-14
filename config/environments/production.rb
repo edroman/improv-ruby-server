4e93a2c2928ac7c4d80ae242ac7f7f27681f6af2
Ed Roman <edward.w.roman@gmail.com>
2012-04-13 19:46:15 -0700

    heroku updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,5 @@ Improv::Application.configure do
   config.active_support.deprecation = :notify
 
   # For ActiveAdmin
-  #config.action_mailer.default_url_options = { :host => 'TODO' }
+  config.action_mailer.default_url_options = { :host => 'http://morning-dawn-3348.herokuapp.com' }
 end
