e05b2ab15d612e8677446202c95f8ba4f01b6c61
Ed Roman <edward.w.roman@gmail.com>
2012-04-14 02:16:14 -0700

    fixed phone from int to string

--- db/migrate/20120409230719_create_users.rb
+++ db/migrate/20120409230719_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :email
-      t.integer :phone
+      t.text :phone
       t.string :password
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(:version => 20120411184603) do
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "email"
-    t.integer  "phone"
+    t.text     "phone"
     t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
