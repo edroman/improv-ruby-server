32d52059445d1fd51d68bb521e77617c438e3c55
Ed Roman <edward.w.roman@gmail.com>
2012-04-25 04:10:46 -0700

    fixed server ip

--- app/models/story.rb
+++ app/models/story.rb
@@ -88,7 +88,7 @@ class Story < ActiveRecord::Base
     def get_ip
       # heroku - code can't get IP since it's a local IP address, so need to hard-code it
       if Rails.env.production? || Rails.env.staging?
-        return "http://APP_CONFIG['server_host']"
+        return "http://#{APP_CONFIG['server_host']}"
       # local - can't just hardcode localhost, since other machines on local network can't access this one, so need code
       else
         require 'socket'
