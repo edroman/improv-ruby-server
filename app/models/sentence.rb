cfcb15252abcdcf7e08e2ccd36fc7643b0c0a30d
Ed Roman <edward.w.roman@gmail.com>
2012-04-27 09:16:45 -0700

    fixed stories/constraints in migration

---
+++ app/models/sentence.rb
@@ -0,0 +1,21 @@
+class Sentence < ActiveRecord::Base
+
+  validate :validate_sentence
+
+  def validate_sentence
+    # Check if sentence has constraint inside it. If not, display an error.
+    # TODO: the error is not being propagated up.  story.curr_sentence doesn't check in-memory models with errors!
+    if (self.body && (self.body.length < 0 || !self.body.downcase.match(self.constraint.downcase)))
+      errors.add :sentence, "must use the word #{self.constraint}!"
+    end
+end
+
+  def body=(body)
+    self[:body] = body
+
+    return if body == nil || body.length == 0
+
+    last_letter = body[body.length-1]
+    self[:body] += "." if !last_letter.match(/[.?!]/)
+  end
+end
