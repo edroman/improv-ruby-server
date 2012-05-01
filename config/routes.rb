5ad28fa74481534627bc0cd5611ae43989d74dc1
Ed Roman <edward.w.roman@gmail.com>
2012-05-01 15:02:18 -0700

    improved facebook login process on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Improv::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   authenticated :user do
     root to: 'stories#index'
