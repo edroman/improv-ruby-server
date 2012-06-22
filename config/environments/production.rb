3bde88a9680286eb38e13681027e92debdb16f10
Ed Roman <edward.w.roman@gmail.com>
2012-06-21 21:22:11 -0700

    switched little to tall tale'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Improv::Application.configure do
   config.active_support.deprecation = :notify
 
   # For ActiveAdmin
-  config.action_mailer.default_url_options = { :host => 'alittletale.com' }
+  config.action_mailer.default_url_options = { :host => 'atalltale.com' }
 
   # For Devise OmniAuth
   config.facebook_token = '390389744334816'
--- lib/tasks/deploy.rake
+++ lib/tasks/deploy.rake
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 namespace :deploy do
-  PRODUCTION_APP = 'alittletale'
+  PRODUCTION_APP = 'atalltale'
   STAGING_APP = 'improv-staging'
   ENV['SKIP_TESTS'] = "true"   # TODO: DELETE
   ENV['SKIP_BACKUP'] = "true"   # TODO: DELETE
