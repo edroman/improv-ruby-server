923f73692cddfa78f1f0366d8d2639211de71e82
Ed Roman <edward.w.roman@gmail.com>
2012-11-24 22:11:31 -0800

    parse improvements

--- voting/routes/index.js
+++ voting/routes/index.js
@@ -5,11 +5,30 @@ function parse(req, res)
 	var Parse = require('parse').Parse;
 	Parse.initialize("oqMegxam44o7Bnqw0osiRGEkheO9aMHm7mEGrKhb", "TzhNqjKrx2TOpvVqNEh3ppBJmcqMUkBq9AMvBjxi");
 
+	// Declaring our object model via sublclassing Parse objects.  We can optionally add instance methods / class methods too.
+	var Turn = Parse.Object.extend("Turn",
+	{
+	});
+	var Turns = Parse.Collection.extend(
+	{
+		model: Turn
+	});
+	var Game = Parse.Object.extend("Game",
+	{
+		turns: Turns
+	});
+	var Games = Parse.Collection.extend(
+	{
+		model: Game,
+		query: new Parse.Query("Game")
+	});
+	
+	// Instantiate the game tree
+	var games = new Games();
+
 	async.waterfall([
 		// 1) Find all games
 		function(callback) {
-			var gameQuery = new Parse.Query("Game");
-			var games = gameQuery.collection();
 			games.fetch(
 			{
 				success:
@@ -20,17 +39,29 @@ function parse(req, res)
 						{
 							// Find all turns related to this game
 							console.log("Game ID: " + game.id);
-							var key = game.id;
-							var turnQuery = new Parse.Query("Turn");
-							// var turns = turnQuery.collection();
-							var turns = turnQuery.equalTo("Game", key).collection();
-							turns.fetch(
+							game.turns = new Parse.Query("Turn").equalTo("Game", game).collection();
+							game.turns.fetch(
 							{
 								success:
 									function(turns)
 									{
 										// For each turn...
-										turns.each( function(turn) { console.log("Turn ID: " + turn.id) } );
+										var i = 0;
+										turns.each( function(turn)
+										{
+											i = i + 1;
+											console.log(i);
+											console.log("Game ID: " + turn.get("Game").id + " Turn ID: " + turn.id)
+										
+											// If we've done an async call to retrieve the last turn of the last game,
+											// then invoke callback so we reply with HTTP response
+											console.log("turn = " + turn + " turns.last = " + turns.last());
+											console.log("game = " + game + " games.last = " + games.last());
+											if (turn == turns.last() && game == games.last())
+											{
+												callback(null, games);
+											}
+										});
 									},
 								error: function(collection, error) { console.log(error); }
 							});
@@ -38,7 +69,6 @@ function parse(req, res)
 					},
 				error: function(collection, error) {  console.log(error); }
 			});
-			callback(null, games);
 		},
 /*
 		// 1) Find all games
